d48f55c8e78e0207d83b2e7eb8a409095538144d
ChiuZhenHuang <cc45001127@gmail.com>
2025-05-05 18:35:29 +0800

    添加測試分支

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -120,9 +120,8 @@ const handleLogin = async () => {
       tomorrow.setDate(tomorrow.getDate() + 1);
       document.cookie = `token=${token};expires=${tomorrow.toUTCString()}`;
       document.cookie = `uid=${uid};expires=${tomorrow.toUTCString()}`;
-
-      router.push({ path: 'layout/home', state: { msg: '登入成功！' } });
       console.log('登入成功:', result);
+      router.push({ path: 'layout/home', state: { msg: '登入成功！' } });
     } else {
       console.log('登入失敗:', result.error);
       messageStore.show('登入失敗', 'error', 3000);
